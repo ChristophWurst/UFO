cdb7c7a4b945f78bb6c8e7c4ee9236ca981c5e0a
Christoph Wurst <christoph@winzerhof-wurst.at>
2015-11-03 17:09:04 +0100

    domain classes added

---
+++ DomainClasses/Area.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFO.DomainClasses {
+
+    [Serializable]
+    public class Area {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString() {
+            return "[" + Id.ToString() + "] " + Name;
+        }
+    }
+}
---
+++ DomainClasses/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFO.DomainClasses {
+
+    [Serializable]
+    public class Location {
+        public int Id { get; set; }
+        public int AreaId { get; set; }
+        public string Name { get; set; }
+        public long Latitude { get; set; }
+        public long Longitude { get; set; }
+
+        public override string ToString() {
+            return "[" + Id.ToString() + "," + AreaId.ToString() + "] " + Name;
+        }
+    }
+}
