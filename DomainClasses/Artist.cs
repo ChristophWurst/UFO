0999dd93d05c09b85db8a5c8ff86e2beb0cf3389
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-11-10 10:13:08 +0100

    artist name only

--- DomainClasses/Artist.cs
+++ DomainClasses/Artist.cs
@@ -5,15 +5,14 @@ namespace UFO.DomainClasses {
     [Serializable]
     public class Artist {
         public int Id { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
+        public string Name { get; set; }
         public string Country { get; set; }
         public string Image { get; set; }
         public string Video { get; set; }
         public int CategoryId { get; set; }
 
         public override string ToString() {
-            return "[" + Id.ToString() + "] " + Firstname + " " + Lastname;
+            return "[" + Id.ToString() + "] " + Name;
         }
     }
 }
