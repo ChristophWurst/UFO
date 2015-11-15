cf5558c769625a8f379f9a9d66b2451b4c83886c
Christoph Wurst <christoph@winzerhof-wurst.at>
2015-11-15 11:32:42 +0100

    category dao added and implemented

---
+++ DALIndependent/CategoryDAO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DAL.Common;
+using UFO.DomainClasses;
+
+namespace UFO.DAL.Independent {
+    class CategoryDAO : ICategoryDAO {
+
+        private const string SQL_SELECT_ALL = "SELECT * "
+                                            + "FROM `category` ";
+
+        private const string SQL_SELECT = "SELECT * "
+                                        + "FROM `category` "
+                                        + "WHERE `id` = @Id";
+
+        private DbCommand createSelectAllCommand() {
+            return db.CreateCommand(SQL_SELECT_ALL);
+        }
+
+        private DbCommand createSelectByIdCommand(int id) {
+            DbCommand cmd = db.CreateCommand(SQL_SELECT);
+            db.DefineParameter(cmd, "@Id", DbType.Int32, id);
+            return cmd;
+        }
+
+        private IDatabase db;
+
+        public CategoryDAO(IDatabase db) {
+            this.db = db;
+        }
+
+        private Category createCategoryFromReader(IDataReader reader) {
+            return new Category() {
+                Id = (int)reader["id"],
+                Description = (string)reader["description"]
+            };
+        }
+
+        public IEnumerable<Category> GetAll() {
+            var categories = new List<Category>();
+            DbCommand cmd = createSelectAllCommand();
+            using (IDataReader reader = db.ExecuteReader(cmd)) {
+                while (reader.Read()) {
+                    categories.Add(createCategoryFromReader(reader));
+                }
+            }
+            return categories;
+        }
+
+        public Category GetById(int id) {
+            Category category = null;
+            DbCommand cmd = createSelectByIdCommand(id);
+            using (IDataReader reader = db.ExecuteReader(cmd)) {
+                if (reader.Read()) {
+                    category = createCategoryFromReader(reader);
+                }
+            }
+            return category;
+        }
+    }
+}
