07c36f4e76efcdd2822574396bced55119397158
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-11-18 20:31:22 +0100

    vernueTest und performanceTest

--- DomainClasses/Venue.cs
+++ DomainClasses/Venue.cs
@@ -8,8 +8,8 @@ namespace UFO.DomainClasses {
         public int AreaId { get; set; }
         public string Desc { get; set; }
         public string ShortDesc { get; set; }
-        public float Latitude { get; set; }
-        public float Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
 
         public override string ToString() {
             return "[" + Id.ToString() + "] " + ShortDesc + " " + Desc;
