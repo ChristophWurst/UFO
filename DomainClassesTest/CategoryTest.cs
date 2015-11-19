ca13a6c63d4e99b81833ee1031151172a4411b9d
Christoph Wurst <christoph@winzerhof-wurst.at>
2015-11-19 15:48:59 +0100

    Category ToString unit test added

---
+++ DomainClassesTest/CategoryTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DomainClasses;
+
+namespace UFO.Test.DomainClasses {
+
+    [TestFixture]
+    class CategoryTest {
+
+        private Category category;
+
+        [SetUp]
+        public void SetUp() {
+            category = new Category();
+        }
+
+        [Test]
+        public void TestId() {
+            int id = 4;
+
+            category.Id = id;
+            Assert.AreEqual(id, category.Id);
+        }
+
+        [Test]
+        public void TestDescription() {
+            string desc = "Test description";
+
+            category.Description = desc;
+            Assert.AreEqual(desc, category.Description);
+        }
+
+        [Test]
+        public void TestToString() {
+            category.Id = 3;
+            category.Description = "Test Category";
+
+            string expected = "[3] Test Category";
+            Assert.AreEqual(expected, category.ToString());
+        }
+
+    }
+
+}
