2dc57a2e19cfdc7f3c2769b8cb5672b85a6973a1
Christoph Wurst <christoph@winzerhof-wurst.at>
2015-11-19 15:48:40 +0100

    Country unit tests added

---
+++ DomainClassesTest/CountryTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DomainClasses;
+
+namespace UFO.Test.DomainClasses {
+
+    [TestFixture]
+    class CountryTest {
+
+        private Country country;
+
+        [SetUp]
+        public void SetUp() {
+            country = new Country();
+        }
+
+        [Test]
+        public void TestId() {
+            int id = 11;
+
+            country.Id = id;
+            Assert.AreEqual(id, country.Id);
+        }
+
+        [Test]
+        public void TestName() {
+            string name = "Austria";
+
+            country.Name = name;
+            Assert.AreEqual(name, country.Name);
+        }
+
+        [Test]
+        public void TestToString() {
+            country.Id = 14;
+            country.Name = "Canada";
+
+            Assert.AreEqual("[14] Canada", country.ToString());
+        }
+
+    }
+
+}
