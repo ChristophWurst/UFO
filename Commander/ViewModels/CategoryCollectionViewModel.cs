f0e88b986e7edb2ab3d1f11a1d62de0ae398c721
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 20:15:19 +0100

    added CategoryCollectionViewModel

---
+++ Commander/ViewModels/CategoryCollectionViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DomainClasses;
+
+namespace UFO.Commander.ViewModels {
+
+	internal class CategoryCollectionViewModel : INotifyPropertyChanged {
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public CategoryCollectionViewModel() {
+			Items = new ObservableCollection<CategoryViewModel>();
+			Items.Add(new CategoryViewModel(new Category() { Id = 1, Description = "BDSM" }));
+			Items.Add(new CategoryViewModel(new Category() { Id = 1, Description = "SM" }));
+			Items.Add(new CategoryViewModel(new Category() { Id = 1, Description = "GONZO" }));
+		}
+
+		public ObservableCollection<CategoryViewModel> Items { get; set; }
+
+		public CategoryViewModel CurrentItem {
+			get { return CurrentItem; }
+			set {
+				if (CurrentItem != value) {
+					CurrentItem = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentItem)));
+				}
+			}
+		}
+	}
+}
