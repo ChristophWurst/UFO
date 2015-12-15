c169a651db69a5f6889d89783d844d195c195ec0
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 09:03:12 +0100

    spectacleday interface added

---
+++ DALCommon/ISpectacledayDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DomainClasses;
+
+namespace UFO.DAL.Common {
+
+	internal interface ISpectacledayDAO {
+
+		Spectacleday GetById(int id);
+
+		IEnumerable<Spectacleday> GetAll();
+	}
+}
