17e34cd79aa5a79f0d32cd171ef9d8234f68d309
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 09:14:56 +0100

    SpectacledayDAOTest added

---
+++ DALMySqlTest/SpectacledayDAOTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using UFO.DAL.Common;
+using UFO.DomainClasses;
+
+namespace DALMySqlTest {
+
+	internal static class SpectacledayExtension {
+
+		public static bool IsEqualTo(this Spectacleday s1, Spectacleday s2) {
+			return s1.Id == s2.Id &&
+				s1.day == s2.day;
+		}
+	}
+
+	[TestFixture]
+	internal class SpectacledayDAOTest {
+		private TransactionScope transaction;
+		private IDatabase db;
+		private ISpectacledayDAO dao;
+		private Spectacleday spectacleday;
+
+		[SetUp]
+		public void SetUp() {
+			transaction = new TransactionScope();
+			DALFactory factory = DALFactory.GetInstance();
+			db = factory.CreateDatabase();
+		}
+	}
+}
