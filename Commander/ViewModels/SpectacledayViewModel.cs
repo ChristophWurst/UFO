010fc501b858909af4715bf2257ebe3f58244c84
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 19:27:01 +0100

    SpectacledayViewModel added

---
+++ Commander/ViewModels/SpectacledayViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DomainClasses;
+
+namespace UFO.Commander.ViewModels {
+
+	internal class SpectacledayViewModel : INotifyPropertyChanged {
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private Spectacleday spectacleday;
+
+		public SpectacledayViewModel(Spectacleday spectacleday) {
+			this.spectacleday = spectacleday;
+		}
+
+		public DateTime Day {
+			get { return spectacleday.Day; }
+			set {
+				if (spectacleday.Day != value) {
+					spectacleday.Day = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(spectacleday.Day)));
+				}
+			}
+		}
+	}
+}
