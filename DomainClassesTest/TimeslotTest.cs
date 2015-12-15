95fc1a7b58f75b22f2d19b6d6c7ac4610bf9190d
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 16:29:16 +0100

    TimeslotTest added

---
+++ DomainClassesTest/TimeslotTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DomainClasses;
+
+namespace UFO.Test.DomainClasses {
+
+	[TestFixture]
+	internal class TimeslotTest {
+		private Timeslot timeslot;
+
+		[SetUp]
+		public void SetUp() {
+			timeslot = new Timeslot();
+		}
+
+		[Test]
+		public void TestId() {
+			int id = 4;
+			timeslot.Id = id;
+			Assert.AreEqual(id, timeslot.Id);
+		}
+
+		[Test]
+		public void TestStart() {
+			TimeSpan ts = new TimeSpan(1, 2, 3);
+			timeslot.Start = ts;
+			Assert.AreEqual(ts, timeslot.Start);
+		}
+
+		[Test]
+		public void TestEnd() {
+			TimeSpan ts = new TimeSpan(3, 2, 1);
+			timeslot.End = ts;
+			Assert.AreEqual(ts, timeslot.End);
+		}
+
+		[Test]
+		public void TestToString() {
+			timeslot.Id = 3;
+			timeslot.Start = new TimeSpan(1, 2, 3);
+			timeslot.End = new TimeSpan(3, 2, 1);
+
+			string expected = "[3] 01:02:03 03:02:01";
+			Assert.AreEqual(expected, timeslot.ToString());
+		}
+	}
+}
