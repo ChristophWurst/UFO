55c374e6706cbdadf5d655d1d645de5059df6650
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 17:05:19 +0100

    TimeslotDAO added

---
+++ DALMySql/TimeslotDAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DAL.Common;
+using UFO.DomainClasses;
+
+namespace UFO.DAL.MySql {
+
+	public class TimeslotDAO : ITimeslotDAO {
+		private IDatabase db;
+
+		public TimeslotDAO(IDatabase db) {
+			this.db = db;
+		}
+
+		public IEnumerable<Timeslot> GetAll() {
+			throw new NotImplementedException();
+		}
+
+		public Timeslot GetById(int id) {
+			throw new NotImplementedException();
+		}
+	}
+}
