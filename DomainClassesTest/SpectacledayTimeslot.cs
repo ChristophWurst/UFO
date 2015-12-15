e1a878d6985ae9356ddee4cbd2157119c5ba77c1
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 16:33:21 +0100

    SpectacledayTimeslot added

---
+++ DomainClassesTest/SpectacledayTimeslot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFO.Test.DomainClasses {
+
+	[Serializable]
+	public class SpectacledayTimeslot {
+		public int Id { get; set; }
+		public int TimeslotId { get; set; }
+		public int SpectacledayId { get; set; }
+
+		public override string ToString() {
+			return "[" + Id.ToString() + "] TimeslotId: " + TimeslotId + "SpectacledayId: " + SpectacledayId;
+		}
+	}
+}
