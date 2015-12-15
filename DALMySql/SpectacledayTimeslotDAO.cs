a1f017691d465e42162478d6b1ccd14a6661860f
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 17:07:39 +0100

    SpectacledayTimeslotDAO added

---
+++ DALMySql/SpectacledayTimeslotDAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DAL.Common;
+using UFO.DomainClasses;
+
+namespace UFO.DAL.MySql {
+
+	public class SpectacledayTimeslotDAO : ISpectacledayTimeslotDAO {
+		private IDatabase db;
+
+		public SpectacledayTimeslotDAO(IDatabase db) {
+			this.db = db;
+		}
+
+		public IEnumerable<SpectacledayTimeslot> GetAll() {
+			throw new NotImplementedException();
+		}
+
+		public SpectacledayTimeslot GetById(int id) {
+			throw new NotImplementedException();
+		}
+	}
+}
