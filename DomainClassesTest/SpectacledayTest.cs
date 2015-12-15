d5e5e569042bf5ca84d3d85614454acabe2c9ecc
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 09:00:23 +0100

    spectacleday domain class added

---
+++ DomainClassesTest/SpectacledayTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DomainClasses;
+
+namespace UFO.Test.DomainClasses {
+
+	[TestFixture]
+	internal class SpectacledayTest {
+		private Spectacleday spectacleday;
+
+		[SetUp]
+		public void SetUp() {
+			spectacleday = new Spectacleday();
+		}
+
+		[Test]
+		public void TestId() {
+			int id = 4;
+			spectacleday.Id = id;
+			Assert.AreEqual(id, spectacleday.Id);
+		}
+
+		[Test]
+		public void TestDay() {
+			DateTime day = new DateTime(1985, 11, 13);
+			spectacleday.day = day;
+			Assert.AreEqual(day, spectacleday.day);
+		}
+
+		[Test]
+		public void TestToString() {
+			spectacleday.Id = 1;
+			spectacleday.day = new DateTime(1985, 11, 13);
+
+			Console.WriteLine(spectacleday.ToString());
+			Assert.AreEqual("[1] 13.11.1985 00:00:00", spectacleday.ToString());
+		}
+	}
+}
