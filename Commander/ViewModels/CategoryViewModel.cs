1adb7711ec0757d3637eb4418c54bd55efe1f79a
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 19:35:40 +0100

    CategoryViewModel added

---
+++ Commander/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DomainClasses;
+
+namespace UFO.Commander.ViewModels {
+
+	internal class CategoryViewModel : INotifyPropertyChanged {
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private Category category;
+
+		public CategoryViewModel(Category category) {
+			this.category = category;
+		}
+
+		public String Description {
+			get { return category.Description; }
+			set {
+				if (category.Description != value) {
+					category.Description = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(category.Description)));
+				}
+			}
+		}
+	}
+}
