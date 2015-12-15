d7ea7394d7fc996a397e2cef9ecd36e8e1b3cdb1
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-15 16:35:49 +0100

    moved SpectacledayTimeslot

--- DomainClasses/SpectacledayTimeslot.cs
+++ DomainClasses/SpectacledayTimeslot.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UFO.Test.DomainClasses {
+namespace UFO.DomainClasses {
 
 	[Serializable]
 	public class SpectacledayTimeslot {
