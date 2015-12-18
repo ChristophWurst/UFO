513c29a3a6ece62821323b498cb603bd9144f564
Stefan Roesch <stefan.roesch1985@gmail.com>
2015-12-18 20:50:40 +0100

    added mail service interface

---
+++ UFO.BL/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UFO.DomainClasses;
+
+namespace UFO.BL {
+
+	internal interface IMailService {
+
+		void MailArtistsNewProgramm(IEnumerable<Artist> artists);
+	}
+}
